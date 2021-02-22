4db2d7d83d618bf43eb3aa5ad5de61efeb902ca6
Liam Forsberg <fourzifer@gmail.com>
2021-02-22 19:52:50 +0100

    Fly up using space, fly down using shift

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -9,7 +9,7 @@ public class PickUp : MonoBehaviour
     void OnMouseDown()
     {
         GetComponent<BoxCollider>().enabled = false;
-        GetComponent<Rigidbody>().useGravity = false;
+        //GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDest.position;
         this.transform.parent = GameObject.Find("Destination").transform;
     }
@@ -17,7 +17,7 @@ public class PickUp : MonoBehaviour
     void OnMouseUp()
     {
         this.transform.parent = null;
-        GetComponent<Rigidbody>().useGravity = true;
+        //GetComponent<Rigidbody>().useGravity = true;
         GetComponent<BoxCollider>().enabled = true;
     }
 }
